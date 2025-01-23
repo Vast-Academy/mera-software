4a1b59be67e8609b4f69d33c088f760e13a44ee5
Vast-Academy <syncvap@gmail.com>
2025-01-23 15:06:44 +0530

    footer page fixing

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { FaHome } from "react-icons/fa";
+import { FiFileText } from "react-icons/fi";
+import { LuUsers } from "react-icons/lu";
+import { FiUser } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200'>
-    <div className='container mx-auto p-4'>
-     <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Jasmeet</p>
+   <div>
+     {/* Bottom Navigation */}
+     <div className="fixed bottom-0 left-0 right-0 bg-gray-900 border-t">
+        <div className="flex justify-around py-2">
+          <div className="flex flex-col text-white items-center">
+            <FaHome className="w-6 h-6 " />
+            <span className="text-xs">Home</span>
+          </div>
+          <div className="flex flex-col text-white items-center">
+            <FiFileText className="w-6 h-6" />
+            <span className="text-xs">Orders</span>
+          </div>
+          <div className="flex flex-col text-white items-center">
+            <LuUsers className="w-6 h-6" />
+            <span className="text-xs">Referral</span>
+          </div>
+          <div className="flex flex-col text-white items-center">
+            <FiUser className="w-6 h-6" />
+            <span className="text-xs">Login</span>
+          </div>
+        </div>
+      </div>
     </div>
-  </footer>
+
   )
 }
 
