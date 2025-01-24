fedc655bf0b67ea0089423f46957d10a894309b2
Vast-Academy <syncvap@gmail.com>
2025-01-24 18:33:04 +0530

    favicon icon update

--- frontend/src/components/AppConvertingBanner.js
+++ frontend/src/components/AppConvertingBanner.js
@@ -5,7 +5,7 @@ const AppConvertingBanner = () => {
   return (
     <div>
        {/* Convert Section */}
-       <div className="mx-4 px-4 py-6 bg-blue-800 text-white rounded-lg mb-4">
+       <div className="mx-4 px-4 py-6 h-28 bg-blue-800 text-white rounded-lg mb-4">
         <h2 className="text-lg font-bold mb-2">Convert Website to App</h2>
         <div className='flex flex-row gap-2'>
         <p className="text-gray-300 text-sm">Transform your website into a mobile app</p>
