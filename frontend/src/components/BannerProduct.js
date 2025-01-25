4c81d0331ece6e2c22ae5c242e559a9a8c4f7320
Vast-Academy <syncvap@gmail.com>
2025-01-25 12:42:24 +0530

    ad image change

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -59,7 +59,7 @@ const BannerProduct = () => {
 
   return (
     <div className='container mx-auto px-4 md:mt-5 rounded'>
-      <div className=' h-40 md:h-56 w-full bg-slate-200 relative'>
+      <div className=' h-40 md:h-64 w-full bg-slate-200 relative'>
 
             {/* <div className='absolute z-10 h-full w-full md:flex items-center hidden'>
                 <div className='flex justify-between w-full text-2xl'>
